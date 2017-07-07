d07dfa48dcb3b3b55c37f14fcba1bf385b92c02b
Jmetev1 <jmetevier@gmail.com>
2017-07-07 16:07:54 -0500

    baby steps

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,17 @@
 // YOUR CODE HERE:
+
+
+let app = {
+
+  init: function() {
+
+  },
+  
+  send: function(message) {
+    $.ajax({
+      url: 'http://parse.opspark.hackreactor.com', 
+      type: 'POST',
+      data: JSON.Stringify(message)
+    });
+  }
+};
