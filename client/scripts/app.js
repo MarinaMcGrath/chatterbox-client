6f3895042aad991ed7e44afd1d9eacdc6c6d2c4d
Jmetev1 <jmetevier@gmail.com>
2017-07-08 10:04:23 -0500

    day 2

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,36 +1,50 @@
-// YOUR CODE HERE:
-
 
 let app = {
-
   init: function() {
 
   },
-  
+  server: 'http://parse.opspark.hackreactor.com/chatterbox/classes/messages',  
   send: function(message) {
     $.ajax({
-      url: 'http://parse.opspark.hackreactor.com/chatterbox/classes/message', 
+      url: app.server, 
       type: 'POST',
       data: JSON.stringify(message)
     });
   },
-
   fetch: function() {
     $.ajax({
-      type: 'GET'
+      url: app.server,
+      type: 'GET',
+      success: function(data) {
+        data.results.forEach(e => {
+          app.renderMessage(e);
+        });
+      }
     });
   },
-
   clearMessages: function() {
     $('#chats').empty();
   },
-
+  handleUsernameClick: function() {    
+    if (!app.friends.includes(this.id)) {
+      app.friends.push(this.id);
+    }    
+  },
+  friends: [],
   renderMessage: function(message) {
-    //turn string into a DOM element
-    $('#chats').append();
+    let name = message.username;
+    let text = message.text;    
+    $('#chats').append(`<div id = ${name} class = "username">${name}: ${text} </div>`);
   },
 
   renderRoom: function(room) {
-
+    $('#roomSelect').append('<div>room</div>');
   }
 };
+
+$(document).ready(function() {
+  app.fetch();
+  $('#chats').on('click', '.username', app.handleUsernameClick);
+
+});
+
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -46,6 +46,7 @@ describe('chatterbox', function() {
         app.send(message);
         ajaxOptions = typeof $.ajax.args[0][0] === 'object' ? $.ajax.args[0][0] : $.ajax.args[0][1];
         var result = JSON.parse(ajaxOptions.data);
+        console.log(ajaxOptions)
         expect(result).to.deep.equal(message);
         done();
       });
